fda7dccd197362e995e53fbfd766b4cde32d0d49
luft <araryap@gmail.com>
2024-06-09 15:47:15 +0700

    git clone

--- template.sh
+++ template.sh
@@ -2,10 +2,12 @@
 apt update
 apt install -y libgl1-mesa-glx
 apt install -y libglib2.0-0
-apt install -y python3-pip
+
 pip3 install Flask
 pip3 install numpy
 pip3 install jsonpickle
 pip3 install opencv-python
 
+git clone https://github.com/pramadani/OtomasiAutoScale.git
+cd OtomasiAutoScale
 python3 flask_server.py
